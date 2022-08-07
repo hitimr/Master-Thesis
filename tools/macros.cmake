b1a092c4d7739aca6e37c736c06db1737ef3d178
hitimr <hitimr@gmail.com>
2022-08-08 00:30:06 +0200

    project compiles

--- tools/macros.cmake
+++ tools/macros.cmake
@@ -7,15 +7,10 @@ function(print)
   endforeach()
 endfunction()
 
-macro(append_cmake_prefix_path)
-  list(APPEND CMAKE_PREFIX_PATH ${ARGN})
-  string(REPLACE ";" "|" CMAKE_PREFIX_PATH "${CMAKE_PREFIX_PATH}")
-endmacro()
-
 macro(setup_subproject_path_vars _NAME)
   set(SUBPROJECT_NAME ${_NAME})
 
-  set(SUBPROJECT_INSTALL_PATH ${INSTALL_DIR_ABSOLUTE}/install)
+  set(SUBPROJECT_INSTALL_PATH ${SUBPROJECT_NAME}/install)
 
   set(SUBPROJECT_SOURCE_PATH ${SUBPROJECT_NAME}/source)
   set(SUBPROJECT_STAMP_PATH ${SUBPROJECT_NAME}/stamp)
@@ -31,26 +26,22 @@ macro(build_subproject)
                         "${multiValueArgs}" ${ARGN})
 
   # Setup SUBPROJECT_* variables (containing paths) for this function
-  setup_subproject_path_vars(${BUILD_SUBPROJECT_NAME})
+  set(SUBPROJECT_NAME ${BUILD_SUBPROJECT_NAME})
 
   # Build the actual subproject
   ExternalProject_Add(${SUBPROJECT_NAME}
     PREFIX ${SUBPROJECT_NAME}
-    DOWNLOAD_DIR ${SUBPROJECT_NAME}
-    STAMP_DIR ${SUBPROJECT_STAMP_PATH}
-    SOURCE_DIR ${SUBPROJECT_SOURCE_PATH}
-    BINARY_DIR ${SUBPROJECT_BUILD_PATH}
+    DOWNLOAD_DIR ${SUBPROJECT_NAME}/download
+    STAMP_DIR ${SUBPROJECT_NAME}/stamp
+    SOURCE_DIR ${SUBPROJECT_NAME}/src
+    BINARY_DIR ${SUBPROJECT_NAME}/bin
     URL ${BUILD_SUBPROJECT_URL}
-    LIST_SEPARATOR | # Use the alternate list separator
+    # LIST_SEPARATOR | # Use the alternate list separator
     CMAKE_ARGS
       -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
       -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
       -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
-      -DCMAKE_INSTALL_PREFIX:PATH=${SUBPROJECT_INSTALL_PATH}
-      -DCMAKE_INSTALL_INCLUDEDIR=${CMAKE_INSTALL_INCLUDEDIR}
-      -DCMAKE_INSTALL_LIBDIR=${CMAKE_INSTALL_LIBDIR}
-      -DCMAKE_INSTALL_DOCDIR=${CMAKE_INSTALL_DOCDIR}
-      -DCMAKE_INSTALL_BINDIR=${CMAKE_INSTALL_BINDIR}
+      -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_CURRENT_BINARY_DIR}/${SUBPROJECT_NAME}
       -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}
       ${BUILD_SUBPROJECT_BUILD_ARGS}
     BUILD_COMMAND ${DEFAULT_BUILD_COMMAND}
@@ -59,11 +50,11 @@ macro(build_subproject)
 
   if(BUILD_SUBPROJECT_DEPENDS_ON)
     ExternalProject_Add_StepDependencies(${SUBPROJECT_NAME}
-      configure ${BUILD_SUBPROJECT_DEPENDS_ON}
+      build ${BUILD_SUBPROJECT_DEPENDS_ON}
     )
   endif()
 
   # Place installed component on CMAKE_PREFIX_PATH for downstream consumption
-  append_cmake_prefix_path(${SUBPROJECT_INSTALL_PATH})
-  append_cmake_prefix_path(${SUBPROJECT_BUILD_PATH})
+  list(APPEND CMAKE_PREFIX_PATH ${CMAKE_CURRENT_BINARY_DIR}/${SUBPROJECT_NAME})
+  # list(APPEND CMAKE_PREFIX_PATH ${CMAKE_CURRENT_BINARY_DIR}/${SUBPROJECT_NAME}/include)
 endmacro()
