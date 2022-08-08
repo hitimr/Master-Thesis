7c88bfa0be3e638f319707b4e228082812b0d93a
hitimr <hitimr@gmail.com>
2022-08-08 04:44:50 +0200

    formatting

---
+++ src/util/logging.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <plog/Appenders/ColorConsoleAppender.h>
+#include <plog/Appenders/RollingFileAppender.h>
+#include <plog/Formatters/CsvFormatter.h>
+#include <plog/Formatters/TxtFormatter.h>
+#include <plog/Init.h>
+#include <plog/Log.h>
+
+namespace logging
+{
+void init()
+{
+  static plog::ColorConsoleAppender<plog::TxtFormatterUtcTime>
+      consoleAppender; // TxtFormatter in UTC
+  static plog::RollingFileAppender<plog::CsvFormatterUtcTime> fileAppender(
+      "UtcTime.csv", 10000,
+      2); // CsvFormatter in UTC
+  plog::init(plog::verbose, &consoleAppender).addAppender(&fileAppender);
+}
+
+} // namespace logging
