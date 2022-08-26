1be84d2b79ffe9ed361c70f3e1a82c0758b986a8
hitimr <hitimr@gmail.com>
2022-08-27 01:49:35 +0200

    text

--- src/experiments/precision/precision.hpp
+++ src/experiments/precision/precision.hpp
@@ -1,25 +1,5 @@
 #pragma once
-#include <logging/logging.hpp>
-#include <math/math.hpp>
-
-#include <DataFrame/DataFrame.h>
-#include <DataFrame/DataFrameTypes.h>
-#include <nanovdb/NanoVDB.h>
-#include <nanovdb/util/GridHandle.h>
-#include <nanovdb/util/HDDA.h>
-#include <nanovdb/util/OpenToNanoVDB.h>
-#include <nanovdb/util/Primitives.h>
-#include <nanovdb/util/Ray.h>
-#include <openvdb/math/Ray.h>
-#include <openvdb/openvdb.h>
-#include <openvdb/tools/LevelSetSphere.h>
-
-#include <algorithm>
-#include <fstream>
-#include <iostream>
-#include <numeric>
-#include <string>
-#include <vector>
+#include "stdafx.hpp"
 
 #define SPHERE_RADIUS 50.
 #define SPHERE_CENTER_X 100.
@@ -28,7 +8,7 @@
 #define VOXEL_SIZE 5
 #define HALFWIDTH 25
 
-#define RAY_COUNT 1000
+#define RAY_COUNT 100
 #define RAY_START_Z -50.
 #define RAY_END_Z 50.
 
